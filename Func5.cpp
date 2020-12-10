4ddda9412a43b277460d25c396a9d37052a1abdd
Kim <kim.zyong@yandex.ru>
2020-12-10 23:38:17 +0300

    Funcs

--- Func5.cpp
+++ Func5.cpp
@@ -1,15 +1,31 @@
 #include <iostream>
 using namespace std;
 
+
+void Add(int&, int);
+
+template<typename T>
+void Swap(T& a, T& b){
+    T t = a;
+    a = b;
+    b = t;
+}
+
 int main(){
     char str[256];
     /*for(char i = 0; i < 256; i++){
         cout << i << '\n';
     }*/
+    int a = 3, b = 5;
+    Swap(a, b);
     cin >> str;
 
     str[2] = '\0';
     cout << str << '\n';
 
     cout << "Hello\0world";
+}
+
+void Add(int& a, int b = 1){
+    a += b;
 }
